501990d7d9f7ae08fa4cb0b2a8ea726005fa6f0c
Rafael Nasibullin <rnasibullin@bigland.ru>
2021-02-09 18:07:46 +0600

    add file response method for ItemController

--- controllers/ItemController.php
+++ controllers/ItemController.php
@@ -3,9 +3,9 @@
 namespace app\controllers;
 
 use app\models\SearchForm;
+use app\services\FileManagerInterface;
 use Yii;
 use yii\web\Controller;
-use yii\web\Response;
 
 class ItemController extends Controller
 {
@@ -19,5 +19,16 @@ class ItemController extends Controller
         return $this->render('index', [
             'searchModel'  => $model,
         ]);
+    }
+
+    public function actionFile(FileManagerInterface $manager)
+    {
+        $model = new SearchForm();
+        $items = $model->search(Yii::$app->request->getQueryParams());
+
+        $filename = $manager->getCSV($items);
+        $filePath = 'files/' . $filename;
+
+        return Yii::$app->response->sendFile($filePath);
     }
 }
